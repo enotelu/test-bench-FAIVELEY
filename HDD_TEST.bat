a21cbf5b332dc35df920d05c28ba8db617a1fa87
enotelu <thibault.laval3@gmail.com>
2023-07-23 17:37:03 +0200

    MAJ a tester

--- HDD_TEST.bat
+++ HDD_TEST.bat
@@ -10,6 +10,31 @@ if not exist "%appPath%" (
 	exit /b
 )
 
+:: Partie à tester
+:: ------------o
+
+echo Vérification du nombre de disques durs connectés...
+
+for /f "delims=" %%i in ('wmic diskdrive get caption') do (
+	echo %%i
+)
+
+set "nbDisks=0"
+
+for /f "delims=" %%i in ('wmic diskdrive get caption') do (
+	set "nbDisks=!nbDisks! + 1"
+)
+
+echo Il y a !nbDisks! disques durs connectés.
+
+if !nbDisks! equ 1 (
+	echo Il n'y a qu'un disque dur connecté.
+	pause
+	exit /b
+)
+
+:: ------------o
+
 start "" "%appPath%"
 
 timeout /t 5 >nul
